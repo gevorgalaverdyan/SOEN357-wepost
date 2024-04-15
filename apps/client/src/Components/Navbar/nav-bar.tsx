abe0c0a0b3fc4180bd6bdc45d7a95bc2b5c046d0
ceyhuntopcu <ceyhuntopcu4107@gmail.com>
2024-04-14 23:02:24 -0400

    Refactor NavBar component to conditionally render login and user navigation based on email presence

--- apps/client/src/Components/Navbar/nav-bar.tsx
+++ apps/client/src/Components/Navbar/nav-bar.tsx
@@ -5,6 +5,8 @@ import { routePaths } from "@/Routes/Routes";
 import { UserNav } from "./user-nav";
 
 export function NavBar() {
+  const email = localStorage.getItem("email");
+  console.log(email);
   return (
     <header className="bg-primary flex h-20 w-full shrink-0 items-center px-4 md:px-6 border-b-2">
       <Sheet>
@@ -32,12 +34,13 @@ export function NavBar() {
             >
               Deliveries
             </a>
-            <a
+            {!email && (<a
               className="flex w-full items-center py-2 text-lg font-semibold"
               href={routePaths.login}
             >
               Log in
-            </a>
+            </a>)}
+            
             <a
               className="flex w-full items-center py-2 text-lg font-semibold"
               href={routePaths.signUp}
@@ -70,19 +73,21 @@ export function NavBar() {
         >
           Deliveries
         </a>
-        <a
+        {!email && (<a
           className="font-medium hover:text-gray-200 transition-colors text-white text-xl"
           href={routePaths.login}
         >
           Log in
-        </a>
+        </a>)}
+        
         <a
           className="font-medium hover:text-gray-200 transition-colors text-white text-xl"
           href={routePaths.quotation}
         >
           Find a rate
         </a>
-        <UserNav />
+        {!email && (<UserNav />)}
+        
       </nav>
     </header>
   );
